b8ec6dd5e4763d64dae871bfaed832ff079fa4e2
Dr. Slavchik <35910408+Siniy-Vyacheslav@users.noreply.github.com>
2019-10-03 15:28:29 +0300

    Add files via upload

--- forschool/4.py
+++ forschool/4.py
@@ -1,8 +1,22 @@
 inp = open('INPUT.TXT', 'r')
 outp = open('OUTPUT.TXT', 'w')
-data = inp.readline()
-a = [][]
+n = int(inp.readline())
+a = []
+for i in range(n):
+    a.append([0] * n)
+mark = 1
 
-for i in range(data):
-    for j in range(data):
-        
+for i in range(n):
+    for j in range(i, n - i):
+        a[i][j] = mark
+        a[j][i] = mark
+        a[n - i - 1][j] = mark
+        a[j][n - i - 1] = mark
+    
+    mark += 1
+
+for i in range(len(a)):
+    string = ''
+    for j in range(len(a)):
+        string += str(a[i][j]) + ' '
+    outp.write(string + '\n')
